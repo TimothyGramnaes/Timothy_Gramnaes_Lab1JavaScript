c0db4a254fb5ae0d41b54709a3cb38ba0c689ed7
Timothy Gramnaes <timothy.gramnaes@medieinstitutet.se>
2020-10-28 21:16:57 +0100

    nytt spel med knappar

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+
+/** 
+ * Array for inputs. text-based
+*/
 let userInputs = [];
 
-
+/**
+ * Clears the textbar after submission
+ */
 function clearField() {
     document.getElementById("answer").value = "";
 }
-
+/**
+ * reads the input from the player and displays where the player is
+ */
 function getAnswer() {
-
+/*
 var input = document.getElementById("answer").value;
 
 if (input === "start") {
@@ -30,7 +38,7 @@ if (input === "start") {
     userInputs.push(input);
 
 } 
-
+*/
 
 
 }
@@ -42,8 +50,86 @@ function runGame() {
 }
 */
 
+
+
+
+function runGame() {
+    document.getElementById("intro").classList.toggle("hidden");
+    document.getElementById("rise").classList.toggle("hidden");
+    userInputs.push();
+
+}
+
+function flushToilet() {
+    document.getElementById("flushToilet").classList.toggle("hidden");
+    document.getElementById("rise").classList.toggle("hidden");
+    userInputs.push();
+
+}
+
+function inspectTheDoor() {
+    document.getElementById("inspectTheDoor").classList.toggle("hidden");
+    document.getElementById("rise").classList.toggle("hidden");
+    userInputs.push();
+
+}
+
+function fineFlushToilet() {
+    
+    document.getElementById("flushToilet").classList.toggle("hidden");
+    document.getElementById("inspectTheDoor").classList.toggle("hidden");
+    
+
+}
+
+function nahImGood() {
+    document.getElementById("inspectTheDoor").classList.toggle("hidden");
+    document.getElementById("nahImGood").classList.toggle("hidden");
+
+}
+
 function damn() {
-    document.getElementById("flush-toilet").classList.toggle("hidden");
+    document.getElementById("flushToilet").classList.toggle("hidden");
     document.getElementById("monster").classList.toggle("hidden");
+    userInputs.push();
+
+}
+
+function sayHi() {
+    document.getElementById("monster").classList.toggle("hidden");
+    document.getElementById("sayHi").classList.toggle("hidden");
+    userInputs.push();
+
+}
+
+function potionYes() {
+    document.getElementById("potionYes").classList.toggle("hidden");
+    document.getElementById("sayHi").classList.toggle("hidden");
+    userInputs.push();
+
+}
+
+function potionNo() {
+    document.getElementById("potionNo").classList.toggle("hidden");
+    document.getElementById("sayHi").classList.toggle("hidden");
+    userInputs.push();
+
+}
+
+function thisIsNotApotion() {
+    document.getElementById("potionNo").classList.toggle("hidden");
+    document.getElementById("potionYes").classList.toggle("hidden");
+
+}
+
+function ehmThanks() {
+    document.getElementById("potionYes").classList.toggle("hidden");
+    document.getElementById("ehmThanks").classList.toggle("hidden");
+
+}
+
+function runAway() {
+    document.getElementById("monster").classList.toggle("hidden");
+    document.getElementById("runAway").classList.toggle("hidden");
 
 }
