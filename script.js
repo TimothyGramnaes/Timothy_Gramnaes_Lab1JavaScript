01cb4d9b185da42561521d3bd49ecbf6862ff755
Timothy Gramnaes <timothy.gramnaes@medieinstitutet.se>
2020-10-28 16:48:40 +0100

    Beta version with text

--- script.js
+++ script.js
@@ -14,13 +14,36 @@ if (input === "start") {
     document.getElementById("rise").classList.toggle("hidden");
     userInputs.push(input);
 
-} else if (input == "inspect" && userInputs.includes("start")) {
+} else if (input == "inspect the door" && userInputs.includes("start")) {
     document.getElementById("rise").classList.toggle("hidden");
     document.getElementById("toilet-door").classList.toggle("hidden");
     userInputs.push(input);
 
+} 
+ else if (input == "flush toilet" && userInputs.includes("start")) {
+    if (userInputs.includes("inspect the door")) {
+        document.getElementById("toilet-door").classList.toggle("hidden");
+    }
+
+    document.getElementById("flush-toilet").classList.toggle("hidden");
+    document.getElementById("rise").classList.toggle("hidden");
+    userInputs.push(input);
+
+} 
+
+
+
 }
+/*
+function runGame() {
+    document.getElementById("intro").classList.toggle("hidden");
+    document.getElementById("rise").classList.toggle("hidden");
+    userInputs.push(input);
+}
+*/
 
-
+function damn() {
+    document.getElementById("flush-toilet").classList.toggle("hidden");
+    document.getElementById("monster").classList.toggle("hidden");
 
 }
